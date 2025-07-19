bdd476a6e23f273a1ea80a41a8266fa7f13ddb37
orbanbotond2002 <obotond59@gmail.com>
2025-07-19 14:25:24 +0300

    Project base, spring boot initialization

---
+++ src/main/java/com/OrbanBotond/Personal_Finance_Tracker/PersonalFinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.OrbanBotond.Personal_Finance_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceTrackerApplication.class, args);
+	}
+
+}
