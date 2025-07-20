675548f549fe3ecc2b908fb75f4cdf7ad2cb2471
orbanbotond2002 <obotond59@gmail.com>
2025-07-20 15:23:29 +0300

    probe, with failing test

---
+++ src/test/java/com/OrbanBotond/Personal_Finance_Tracker/FailingTest.java
@@ -0,0 +1,13 @@
+package com.OrbanBotond.Personal_Finance_Tracker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FailingTest {
+
+    @Test
+    void test(){
+        assertEquals(1, 2, "Ez a teszt mindig el fog bukni.");
+    }
+}
