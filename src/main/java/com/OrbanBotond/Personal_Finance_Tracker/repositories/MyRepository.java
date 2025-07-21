758c0ebcfd24263f0dd0f02b4b0a888c2876021c
orbanbotond2002 <obotond59@gmail.com>
2025-07-21 14:56:18 +0300

    final for the initialization

--- src/main/java/com/OrbanBotond/Personal_Finance_Tracker/repositories/MyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.OrbanBotond.Personal_Finance_Tracker.repositories;
-
-import com.OrbanBotond.Personal_Finance_Tracker.entities.MyModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyRepository extends JpaRepository<MyModel, Long> {
-}
